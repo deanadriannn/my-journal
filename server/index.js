bc60ea1b308bb581baed7ce29a440453513bcbeb
deanadriannn <deanadrian644@gmail.com>
2023-06-02 16:53:04 +0700

    menambahkan response send

--- server/index.js
+++ server/index.js
@@ -16,6 +16,10 @@ app.use(express.json({ limit: '50mb' }));
 app.use('/api/journal', journalRoute);
 app.use('/api/user', userRoute);
 
+app.get('/', (req, res) => {
+  res.send('Hello to Journal API')
+})
+
 const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
